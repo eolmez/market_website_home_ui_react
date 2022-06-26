61854585c3a5138072ca5102bf71d58b8c490131
Emre Ölmez <eolmez025@gmail.com>
2022-06-26 09:28:09 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { GlobalStyle } from "./globalStyles";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home";
